10242445dad496dd24b5c68678c16e2cee15ce27
Xie Yuhao <xieyuhao@cmhi.chinamobile.com>
2020-05-08 18:35:59 +0800

    trying to add unit test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+
   root: true,
 
   globals: {
